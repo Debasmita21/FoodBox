43a70c373478fe677bb137fdd484d3cc81aabc2f
Debasmita21 <63196707+Debasmita21@users.noreply.github.com>
2024-05-01 23:53:28 +0530

    added toggle features to signIn/SignUp form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -13,9 +13,9 @@ const Login = (props) => {
       <img className="brightness-75 w-screen h-screen" src= {background_image} alt="img"/>
       </div>
       <div className="my-36 relative">
-        <h1 className=" text-center text-white text-4xl italic font-bold">Food Box</h1>
+        <h1 className=" text-center text-white text-6xl italic font-bold">Food Box</h1>
       
-        <h1 className=" text-center text-white text-4xl">Find the best restaurants, cafés and bars in India</h1>
+        <h1 className=" text-center mt-8 text-white text-4xl">Find the best restaurants, cafés and bars in India</h1>
       </div>
       {showLoginPopup &&<LoginPopup onClose={()=>setshowLoginPopup(false)} />}
     </div>
--- src/Components/LoginPopup.js
+++ src/Components/LoginPopup.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { X } from 'lucide-react';
 
 const LoginPopup = ({onClose}) => {
@@ -8,6 +8,10 @@ const LoginPopup = ({onClose}) => {
     if(modalRef.current === e.target){
         onClose();
     }
+  }
+  const [isSignInForm,setIsSignInForm]=useState(true);
+  const toggleSignIn=()=>{
+    setIsSignInForm(!isSignInForm)
   }
   return (
     <div ref={modalRef} onClick={closeLoginPopup} className='fixed inset-0 bg-blue bg-opacity-30 backdrop-blur-sm 
@@ -20,18 +24,21 @@ const LoginPopup = ({onClose}) => {
            <form className="bg-white px-16 absolute w-full md:w-3/12 h-3/4
            my-32  mx-auto left-0 right-0 text-black bg-opacity-80 flex-shrink-0">
 
-              <h1 className="font-bold text-2xl my-6 p-3">Sign In</h1>
+              <h1 className="font-bold text-2xl my-6 p-3">{isSignInForm?"Sign In":"Sign Up"}</h1>
 
-              <input type="text" placeholder="Full name" 
-               className="p-3 my-3 w-full rounded-sm bg-[#333]" />
+              {!isSignInForm &&<input type="text" placeholder="Full name" 
+               className="p-3 my-3 w-full rounded-sm bg-[#333]" />}
 
               <input type="text" placeholder="Email or phone number" 
                className="p-3 my-3 w-full rounded-sm bg-[#333]" />
 
               <input  type="password" placeholder="Password" 
                 className="p-3 my-3 w-full rounded-sm bg-[#333]"/>
-                <button type="submit" className="p-2 my-8 w-full
-                 bg-rose-800 rounded-sm"> Sign In</button>
+                <button type="submit" className="p-2 my-8 w-full font-bold
+                 bg-rose-800 rounded-sm"> {isSignInForm?"Sign In":"Sign Up"}</button>
+
+                 <span className="cursor-pointer text-blue-700" onClick={toggleSignIn}>
+                 {isSignInForm?"New to Netflix? Sign up now.":"Already registred. Sign in now."}</span>
 
         <div>
         
